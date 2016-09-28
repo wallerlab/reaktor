4f513a7ef675a73ba331b09cb2382c24af71f228
smcanana <smcanana@users.noreply.github.com>
2016-09-28 14:57:07 +0200

    Update publish-javadoc.sh
    corrected project name

--- .utility/publish-javadoc.sh
+++ .utility/publish-javadoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "wallerlab/yoink" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "wallerlab/reaktor" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
   echo -e "Publishing javadoc...\n"
 
@@ -9,7 +9,7 @@ if [ "$TRAVIS_REPO_SLUG" == "wallerlab/yoink" ] && [ "$TRAVIS_JDK_VERSION" == "o
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/wallerlab/yoink gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/wallerlab/reaktor gh-pages > /dev/null
 
   cd gh-pages
   git rm -rf ./javadoc
